d6147faa4ec54c844e1117453811ab3c412c7da2
chenn <chennj_cn@aliyun.com>
2017-09-23 19:31:52 +0700

    second commit

--- app/src/main/java/com/androidexample/chenn/simplehookinstrumentation/MainActivity.java
+++ app/src/main/java/com/androidexample/chenn/simplehookinstrumentation/MainActivity.java
@@ -1,7 +1,10 @@
 package com.androidexample.chenn.simplehookinstrumentation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView tx = (TextView) findViewById(R.id.tx_hook);
+        tx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
     }
 }
